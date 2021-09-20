9c0086ec1783781dd6096a6013e43326ba12feee
sukhdevr898 <59791782+sukhdevr898@users.noreply.github.com>
2021-09-20 13:05:10 +0530

    Update spd.sh

--- spd.sh
+++ spd.sh
@@ -17,7 +17,7 @@ rm Ip.txt
 
 echo -e "\e[94m\n\n ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
 
-#                    speedtest
+             speedtest
 echo -e "\e[94m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
 echo -e "\e[94m             ━━━━━━DONE━━━━━━━ "
 echo -e "\e[94m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
@@ -26,3 +26,9 @@ echo -e "\e[94m ━━━━━━━━━━━━━━━━━━━━━�
 echo -e "\e[94m    DO YOU WANT TO SPEEDTEST AGAIN \n\t\t PRESS ENTER... \n\t\tOTHERWISE TYPE 'n' "
 echo -e "\e[94m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
 read -p " CTRL + C FOR EXIT " spd
+If [ $spd == "n" ];
+then
+bash joker.sh
+else
+bash spd.sh
+fi
