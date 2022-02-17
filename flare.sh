87e9cff7c47869d920f7e814248165c16af90404
sukhdevr898 <59791782+sukhdevr898@users.noreply.github.com>
2022-02-17 13:03:10 +0530

    Create flare.sh

---
+++ flare.sh
@@ -0,0 +1,6 @@
+echo -e "\e[96m        |=====================================================|"
+echo -e "\e[96m        |                      CLOUDFLARED SERVER             |"
+echo -e "\e[96m        |         (  FIND AND COPY LINK THEN SEND   )         |"
+echo -e "\e[96m        |     EXAMPLE https://example-url.trycloudflare.com   |"
+echo -e "\e[96m        |=====================================================|"
+sleep 5.0
